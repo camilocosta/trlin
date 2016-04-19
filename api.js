c02d0688ea8caf1be2a6e310691f9ed4d4cf6576
Camilo Costa <camilogilc@gmail.com>
2016-04-19 00:43:48 -0300

    permitir requests via www.trlin.com

--- api.js
+++ api.js
@@ -22,7 +22,7 @@ var path = require('path');
 var app = express();
 
 app.all('*', function(req, res, next) {
-  res.set('Access-Control-Allow-Origin', 'http://localhost');
+  res.set('Access-Control-Allow-Origin', 'http://www.trlin.com');
   res.set('Access-Control-Allow-Credentials', true);
   res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
